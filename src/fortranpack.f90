96eeb8d611bf8b474828d78fd732ddb3149fda02
Jacob Williams <jacobwilliams@users.noreply.github.com>
2025-04-19 10:23:47 -0500

    sort

--- src/fortranpack.f90
+++ src/fortranpack.f90
@@ -1,36 +1,36 @@
 module fortranpack
 
-  use json_module
-  use numerical_differentiation_module
-  use ddeabm_module
-  use bspline_module
-  use pyplot_module
-  use sorting_module
-  use nlesolver_module
   use argv_module
-  use simulated_annealing_module
-  use regridpack_module
-  use linear_interpolation_module
-  use pchip_module
-  use splpak_module
-  use csv_module
-  use slsqp_module
-  use conmin_module
+  use bspline_module
   use conmax_module
-  use psqp_module
-  use optgra_module
-  use pikaia_module
-  use lbfgsb_module
-  use root_module
-  use polyroots_module
+  use conmin_module
+  use csv_module
+  use dag_module
+  use ddeabm_module
   use dop853_module
-  use rklib_module
+  use json_module
+  use lbfgsb_module
+  use linear_interpolation_module
+  use mach_routines
+  use mt19937_64
+  use nlesolver_module
+  use numerical_differentiation_module
+  use optgra_module
+  use pchip_module
+  use pikaia_module
+  use polyroots_module
+  use popen_module
+  use psqp_module
+  use pyplot_module
   use quadpack
   use quadrature_module
-  use mt19937_64
-  use dag_module
-  use popen_module
-  use mach_routines
+  use regridpack_module
+  use rklib_module
+  use root_module
+  use simulated_annealing_module
+  use slsqp_module
+  use sorting_module
+  use splpak_module
 
   implicit none
 
